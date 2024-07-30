fe07b284ab45d391529fb54b02c0479e3e481b7e
unknown <carlos.sala.munoz@gmail.com>
2024-07-30 09:41:25 -0400

    se actualizan las dependencias

--- app/src/main/java/com/example/imc/ResultIMCActivity.kt
+++ app/src/main/java/com/example/imc/ResultIMCActivity.kt
@@ -1,11 +1,8 @@
 package com.example.imc
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.imc.MainIMCActivity.Companion.IMC_KEY
 import com.example.imc.databinding.ActivityResultImcactivityBinding
 
